abcca19b8067aa70adf765f2dc80d2eebb9226d3
Digambar Chaudhari <digambarchaudhari425303@gmail.com>
2023-02-19 10:53:23 +0530

    done changes

--- lib/widgets/cards.dart
+++ lib/widgets/cards.dart
@@ -13,6 +13,8 @@ class MomTileCrad extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       // color: Color.fromARGB(255, 199, 185, 249),
       child: InkWell(
         onTap: (() {
