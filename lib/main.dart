5d771bccbda0e5d9f1539a3786789070ad88a3df
Pratik Bari <pratikbari2222@gmail.com>
2023-03-09 23:56:44 +0530

    excercise page done added the const modifier in many files Changes to be committed: 	deleted:    assets/images/M_maternity_cloths.png 	new file:   assets/images/lady.jpg 	new file:   assets/images/mE_lowerBody.png 	new file:   assets/images/mE_meditation.png 	new file:   assets/images/mE_upperBody.png 	new file:   assets/images/mE_yoga.png 	new file:   assets/images/mS_breastPump.jpg 	deleted:    assets/images/m_clothing.png 	modified:   lib/main.dart 	new file:   lib/mom/m_exercise.dart 	modified:   lib/mom/m_main.dart 	modified:   lib/mom/m_store.dart 	modified:   lib/tools/tools_main.dart 	modified:   lib/utils/routes.dart 	new file:   lib/widgets/exercise_cards.dart 	modified:   lib/widgets/store_link_card.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,24 @@ import 'package:pregcare/utils/routes.dart';
 import 'mom/m_articles.dart';
 import 'mom/m_experience.dart';
 import 'mom/m_store.dart';
+import 'mom/m_exercise.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.pink, statusBarIconBrightness: Brightness.light));
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
-        appBarTheme: AppBarTheme(
+        appBarTheme: const AppBarTheme(
           color: Colors.pinkAccent,
         ),
         bottomSheetTheme: BottomSheetThemeData(
@@ -29,12 +32,13 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: "/",
       routes: {
-        "/": (context) => Pregcare(),
+        "/": (context) => const Pregcare(),
         PregcareRoutes().momWeightRoute: (context) => const MWeight(),
         PregcareRoutes().momArticleRoute: (context) => const ArticlesPage(),
         PregcareRoutes().momExperienceRoute: (context) =>
             const ExperiencesPage(),
-        PregcareRoutes().momStoreRoute: (context) => const Mstore()
+        PregcareRoutes().momStoreRoute: (context) => const Mstore(),
+        PregcareRoutes().momExerciseRoute: (context) => MExercise()
       },
     );
   }
--- lib/tools/tools_main.dart
+++ lib/tools/tools_main.dart
@@ -12,7 +12,7 @@ class _MyToolsState extends State<MyTools> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 238, 238, 238),
+      backgroundColor: const Color.fromARGB(255, 238, 238, 238),
       appBar: AppBar(
         backgroundColor: Colors.pink,
         // center might be temporarily here
@@ -29,9 +29,9 @@ class _MyToolsState extends State<MyTools> {
         // component 1--> SOS Button
         child: Column(
           children: [
-            Center(),
+            const Center(),
             Container(
-              padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+              padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
               child: Material(
                 elevation: 8,
                 borderRadius: BorderRadius.circular(100),
@@ -40,11 +40,11 @@ class _MyToolsState extends State<MyTools> {
                   splashColor: Colors.black54,
                   onTap: () {},
                   child: Ink.image(
-                    image: AssetImage('assets/images/redButton.png'),
+                    image: const AssetImage('assets/images/redButton.png'),
                     height: 200,
                     width: 225,
                     fit: BoxFit.cover,
-                    child: Center(
+                    child: const Center(
                       child: Text(
                         'SOS',
                         style: TextStyle(fontSize: 30, color: Colors.white),
@@ -63,7 +63,7 @@ class _MyToolsState extends State<MyTools> {
                       150, // Container child widget will get this width value
                   height:
                       225, // Container child widget will get this height value
-                  padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
                   child: Material(
                     color: Colors.white,
                     elevation: 8,
@@ -75,20 +75,21 @@ class _MyToolsState extends State<MyTools> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          SizedBox(
+                          const SizedBox(
                             height: 20,
                           ),
-                          Center(),
+                          const Center(),
                           Ink.image(
-                            image: AssetImage('assets/images/location_t.png'),
+                            image: const AssetImage(
+                                'assets/images/location_t.png'),
                             height: 100,
                             width: 200,
                             fit: BoxFit.cover,
                           ),
-                          SizedBox(
+                          const SizedBox(
                             height: 20,
                           ),
-                          Text(
+                          const Text(
                             'Get Location',
                             textAlign: TextAlign.center,
                             style: TextStyle(fontSize: 20, color: Colors.black),
@@ -103,7 +104,7 @@ class _MyToolsState extends State<MyTools> {
                       150, // Container child widget will get this width value
                   height:
                       225, // Container child widget will get this height value
-                  padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
                   child: Material(
                     color: Colors.white,
                     elevation: 8,
@@ -115,17 +116,18 @@ class _MyToolsState extends State<MyTools> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          SizedBox(
+                          const SizedBox(
                             height: 20,
                           ),
-                          Center(),
+                          const Center(),
                           Ink.image(
-                            image: AssetImage('assets/images/doctor1.png'),
+                            image:
+                                const AssetImage('assets/images/doctor1.png'),
                             height: 100,
                             width: 200,
                             fit: BoxFit.fitHeight,
                           ),
-                          Center(
+                          const Center(
                             child: Text(
                               'Consult with Doctor',
                               textAlign: TextAlign.center,
@@ -151,7 +153,7 @@ class _MyToolsState extends State<MyTools> {
                       150, // Container child widget will get this width value
                   height:
                       225, // Container child widget will get this height value
-                  padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
                   child: Material(
                     color: Colors.white,
                     elevation: 8,
@@ -163,20 +165,21 @@ class _MyToolsState extends State<MyTools> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          SizedBox(
+                          const SizedBox(
                             height: 20,
                           ),
-                          Center(),
+                          const Center(),
                           Ink.image(
-                            image: AssetImage('assets/images/m_calender.png'),
+                            image: const AssetImage(
+                                'assets/images/m_calender.png'),
                             height: 100,
                             width: 200,
                             fit: BoxFit.fitHeight,
                           ),
-                          SizedBox(
+                          const SizedBox(
                             height: 20,
                           ),
-                          Text(
+                          const Text(
                             'Important Dates',
                             textAlign: TextAlign.center,
                             style: TextStyle(fontSize: 20, color: Colors.black),
@@ -191,7 +194,7 @@ class _MyToolsState extends State<MyTools> {
                       150, // Container child widget will get this width value
                   height:
                       225, // Container child widget will get this height value
-                  padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
                   child: Material(
                     color: Colors.white,
                     elevation: 8,
@@ -203,19 +206,20 @@ class _MyToolsState extends State<MyTools> {
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
                         children: [
-                          SizedBox(
+                          const SizedBox(
                             height: 20,
                           ),
                           Ink.image(
-                            image: AssetImage('assets/images/smart_watch.png'),
+                            image: const AssetImage(
+                                'assets/images/smart_watch.png'),
                             height: 100,
                             width: 200,
                             fit: BoxFit.fitHeight,
                           ),
-                          SizedBox(
+                          const SizedBox(
                             height: 20,
                           ),
-                          Text(
+                          const Text(
                             'Smart Watch',
                             textAlign: TextAlign.center,
                             style: TextStyle(fontSize: 20, color: Colors.black),
