6ef41c7c6975f7f93c036b1c5b9a5260eaa8cf3f
Pratik Bari <pratikbari2222@gmail.com>
2023-03-11 22:42:21 +0530

    minor bug fixes in main Changes to be committed:

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,14 @@ import 'mom/m_experience.dart';
 import 'mom/m_store.dart';
 import 'mom/m_exercise.dart';
 
-
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+// void main() {
+//   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.pink, statusBarIconBrightness: Brightness.light));
   runApp(const MyApp());
 }
@@ -34,29 +32,25 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
-
         appBarTheme: const AppBarTheme(
           color: Colors.pinkAccent,
-
-        appBarTheme: AppBarTheme(
-          color: Colors.pink,
-
         ),
+        // appBarTheme:AppBarTheme(
+        //   color: Colors.pink,
+        // ),
         bottomSheetTheme: BottomSheetThemeData(
           backgroundColor: Colors.black.withOpacity(0),
         ),
       ),
       initialRoute: "/",
       routes: {
-
         "/": (context) => const Pregcare(),
-        PregcareRoutes().momWeightRoute: (context) => const MWeight(),
-
+        PregcareRoutes().momWeightRoute: (context) => MWeight(),
+        // ignore: equal_keys_in_map
         "/": (context) => LoginController().handleAuthState(),
         PregcareRoutes().homeRoute: (context) => const Pregcare(),
         PregcareRoutes().momWeightRoute: (context) => MWeight(),
         PregcareRoutes().momWeightRoute: (context) => MWeight(),
-
         PregcareRoutes().momArticleRoute: (context) => const ArticlesPage(),
         PregcareRoutes().momExperienceRoute: (context) =>
             const ExperiencesPage(),
